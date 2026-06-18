55e3015c30876ac9defd4a1fc13296f698ee19c3
ethnwr <daniil.gawrilov07@gmail.com>
2026-06-18 19:55:41 +0900

    модуль логирования

---
+++ src/logger.py
@@ -0,0 +1,12 @@
+import logging
+import os
+
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+logging.basicConfig(
+    filename='logs/shop.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger('BookShop')
